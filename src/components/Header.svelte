c90849fbbb0d0481611650d38639e34fe6543b61
olahmyde <37003274+olahmyde@users.noreply.github.com>
2024-01-03 02:26:38 +0000

    Header Navigation completed

---
+++ src/components/Header.svelte
@@ -0,0 +1,30 @@
+<script>
+   export let y;
+   import ChubLogo from "../assets/wLogo_white.png"
+   
+   function downloadApp(){
+    location.href="#downloadApp";
+   }
+   
+   function refreshPage(){
+    window.location.reload();
+   }
+</script>
+
+<header class="w-auto justify-center flex">
+    
+    <div class="header flex justify-between">
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+        <img on:click={refreshPage} class="my-[24px] ml-[80px] bg-transparent " src={ChubLogo} alt='CHUB logo'>
+        
+        <nav class="bg-transparent flex gap-5 my-[40px] self-center">
+            <a class="nav-links" href='#features'>Features</a>
+            <a class="nav-links" href='#integration'>Integration</a>
+            <a class="nav-links" href='#blog'>Blog</a>
+        </nav>
+        
+        <button on:click={downloadApp} class="buttons header-button my-[14px] mr-[16px] self-center"> Get Started</button>
+    </div>
+    
+</header>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<script>
+    import Header from '../components/Header.svelte'
+    import Main from '../components/Main.svelte'
+</script>
